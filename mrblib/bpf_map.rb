702827dedad12db7980bf6854183743132d29fdb
Uchio Kondo <udzura@udzura.jp>
2021-09-17 00:33:39 +0900

    Map is now Enumerable

---
+++ mrblib/bpf_map.rb
@@ -0,0 +1,77 @@
+module BPF
+  class Map
+    def key_packer
+      @key_packer ||= lambda {|v| v}
+    end
+
+    def key_unpacker
+      @key_unpacker ||= lambda {|v| v}
+    end
+
+    def value_unpacker
+      @value_unpacker ||= lambda {|v| v}
+    end
+
+    def key_packer=(blk)
+      @key_packer = blk
+    end
+
+    def key_unpacker=(blk)
+      @key_unpacker = blk
+    end
+
+    def value_unpacker=(blk)
+      @value_unpacker = blk
+    end
+
+    def key_type=(type)
+      if type == Integer
+        self.key_packer = lambda{|k| [k].pack("I") }
+        self.key_unpacker = lambda{|k| k.unpack("I")[0] }
+      end
+    end
+
+    def value_type=(type)
+      if type == Integer
+        self.value_unpacker = lambda{|v| v.unpack("I")[0] }
+      end
+    end
+
+    def each
+      key = nil
+      ret = []
+
+      while key = next_key(key)
+        data = lookup(key)
+        value = value_unpacker.call(data)
+        kv = [key_unpacker.call(key), value]
+        yield *kv
+        ret << kv
+      end
+
+      ret
+    end
+
+    def keys
+      r = []
+      key = nil
+      while key = next_key(key)
+        r << key_unpacker.call(key)
+      end
+      r
+    end
+
+    def to_h
+      ret = {}
+      each {|k, v| ret[k] = v }
+      ret
+    end
+    alias items to_h
+
+    def values
+      to_h.values
+    end
+
+    include Enumerable
+  end
+end
